31761887b922c4984b4964205c2739532023646d
JP Eiler <jp.eiler97@gmail.com>
2021-04-12 16:37:55 -0500

    finished tests for engineer.js

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,3 +1,28 @@
-const Engineer = require("./lib/Engineer");
+const { describe, it, expect } = require("@jest/globals");
+const Engineer = require("../lib/engineer");
 
-describe("Engineer class", () => {});
+describe("Engineer class", () => {
+  it("should take name as a parameter", () => {
+    const testName = "John";
+    const newEngineer = new Engineer("John", 1, "john@john.com", "john123");
+    expect(newEngineer.name).toBe(testName);
+  });
+  it("should take github as a parameter", () => {
+    const testAccount = "john123";
+    const newEngineer = new Engineer("John", 1, "john@john.com", "john123");
+    expect(newEngineer.github).toBe(testAccount);
+  });
+  describe("getGithub", () => {
+    it("should return the github parameter that has been passed in", () => {
+      const testAccount = "john123";
+      const newEngineer = new Engineer("John", 1, "john@john.com", "john123");
+      expect(newEngineer.getGithub()).toBe(testAccount);
+    });
+  });
+  describe("getRole", () => {
+    it("should return the role 'Engineer'", () => {
+      const newEngineer = new Engineer("John", 1, "john@john.com", "john123");
+      expect(newEngineer.getRole()).toBe("Engineer");
+    });
+  });
+});
