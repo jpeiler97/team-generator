06a6667b4aed8600721e3a3915c80ec331be8128
JP Eiler <jp.eiler97@gmail.com>
2021-04-12 15:01:45 -0500

    wrote tests for Employee class

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,3 @@
-const { it } = require("@jest/globals");
-const { describe } = require("yargs");
 const Employee = require("./lib/Employee");
 
 describe("Employee class", () => {
@@ -8,8 +6,31 @@ describe("Employee class", () => {
     expect(new Employee("John", 1, "john@john.com").name).toBe(testName);
   });
 
-  describe("getName", () => {});
-  describe("getId", () => {});
-  describe("getEmail", () => {});
-  describe("getRole", () => {});
+  describe("getName", () => {
+    it("should return the name passed in as a parameter", () => {
+      const testName = "John";
+      const newEmployee = new Employee("John", 1, "john@john.com");
+      expect(newEmployee.getName().toBe(testName));
+    });
+  });
+  describe("getId", () => {
+    it("should return the id passed in as parameter", () => {
+      const testId = "1";
+      const newEmployee = new Employee("John", 1, "john@john.com");
+      expect(newEmployee.getId().toBe(testId));
+    });
+  });
+  describe("getEmail", () => {
+    it("should return the Email passed in as a parameter", () => {
+      const testEmail = "john@john.com";
+      const newEmployee = new Employee("John", 1, "john@john.com");
+      expect(newEmployee.getEmail().toBe(testEmail));
+    });
+  });
+  describe("getRole", () => {
+    it("should return the role 'Employee'", () => {
+      const newEmployee = new Employee("John", 1, "john@john.com");
+      expect(newEmployee.getRole().toBe("Employee"));
+    });
+  });
 });
