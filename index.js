a49045588c0fa89f2a57a7d53670ba9a276d103f
JP Eiler <jp.eiler97@gmail.com>
2021-04-14 11:08:07 -0500

    added code to write HTML file, style.css to /dist

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
 
 const employeeList = [];
+let bodyHTML = ``;
 
 function initialPrompts() {
   inquirer
@@ -120,11 +121,13 @@ function startPrompts() {
                   employeeList.push(newEmployee);
                 }
                 console.log(employeeList);
+
                 startPrompts();
               });
           });
       } else {
-        return;
+        employeeList.forEach(writeHTMLCards);
+        writeHTML();
       }
     });
 }
@@ -133,3 +136,97 @@ console.log("Welcome to the Team Profile Generator!");
 console.log("**************************************\n");
 console.log("Please enter data for your team manager.\n");
 initialPrompts();
+
+const firstHTML = `
+  !DOCTYPE html>
+    <html lang="en">
+      <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+        crossorigin="anonymous"
+      />
+      <link rel="stylesheet" href="style.css" />
+      <title>Team Profile Generator</title>
+    </head>
+    <body>
+      <header>Team Profile</header>
+      <div class="welcome-message">
+        <h2>Welcome to our team!</h2>
+      </div>
+      <div class="profile-area container-fluid">
+      <div class="row my-4 justify-content-center">`;
+
+const lastHTML = `
+    </div>
+    </div>
+  </div>
+  </body>
+</html>`;
+
+function writeHTML() {
+  fs.writeFile("./dist/index.html", firstHTML, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+  fs.appendFile("./dist/index.html", bodyHTML, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+  fs.appendFile("./dist/index.html", lastHTML, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
+
+function writeHTMLCards(employee, i, array) {
+  if (i / 3 === 1 && i !== array.length) {
+    bodyHTML += `</div>
+      <div class="row my-4 justify-content-center">\n`;
+  }
+
+  if (employee.getRole() === "Manager") {
+    bodyHTML += `
+      <div class="col-3">
+      <div class="card">
+        <div class="card-header">Manager</div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Name: ${employee.getName()}</li>
+          <li class="list-group-item">ID #: ${employee.getId()}</li>
+          <li class="list-group-item">Email: ${employee.getEmail()}</li>
+          <li class="list-group-item">Office number: ${employee.getOfficeNumber()}</li>
+        </ul>
+      </div>
+    </div>\n`;
+  }
+
+  if (employee.getRole() === "Intern") {
+    bodyHTML += `
+      <div class="col-3">
+      <div class="card">
+        <div class="card-header">Intern</div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Name: ${employee.getName()}</li>
+          <li class="list-group-item">ID #: ${employee.getId()}</li>
+          <li class="list-group-item">Email: ${employee.getEmail()}</li>
+          <li class="list-group-item">School: ${employee.getSchool()}</li>
+        </ul>
+      </div>
+    </div>\n`;
+  }
+  if (employee.getRole() === "Engineer") {
+    bodyHTML += `
+      <div class="col-3">
+      <div class="card">
+        <div class="card-header">Engineer</div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Name: ${employee.getName()}</li>
+          <li class="list-group-item">ID #: ${employee.getId()}</li>
+          <li class="list-group-item">Email: ${employee.getEmail()}</li>
+          <li class="list-group-item">GitHub: ${employee.getGithub()}</li>
+        </ul>
+      </div>
+    </div>\n`;
+  }
+}
